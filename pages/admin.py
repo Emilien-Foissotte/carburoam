b7c7864e70a6710bf277fb628373a5f0f22cd80c
Emilien Foissotte <emilienfoissotte44@gmail.com>
2024-05-10 12:18:42 +0200

    fix: declare query as explicit text

--- pages/admin.py
+++ pages/admin.py
@@ -78,7 +78,7 @@ if st.session_state["authentication_status"]:
                         logger.info("User modified the database")
                         conn = st.connection("gas_db", type="sql")
                         with conn.session as s:
-                            result = s.execute(query)
+                            result = s.execute(text(query))
                             st.write(result)
                             s.commit()
             except Exception as e:
