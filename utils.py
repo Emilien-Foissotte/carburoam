31bf6747f50dee833ca447c442c9f533f1c4596b
Emilien Foissotte <emilienfoissotte44@gmail.com>
2025-01-18 18:38:50 +0100

    feat: bump version

--- utils.py
+++ utils.py
@@ -29,7 +29,7 @@ from yaml.loader import SafeLoader
 from models import CustomStation, GasType, Price, Station, Transfer, User
 from session import db_session
 
-VERSION = "0.2.0"
+VERSION = "0.3.0"
 
 #################
 ##AUTHENTICATOR##
