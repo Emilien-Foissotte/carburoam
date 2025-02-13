7f0707dbb4d7d6d59cffac1fbee46de14a9dceb7
Emilien Foissotte <emilienfoissotte44@gmail.com>
2025-02-13 12:28:32 +0100

    feat: mock the config file to ensure no exception is raised by app

--- test_root.py
+++ test_root.py
@@ -1,16 +1,42 @@
+from pathlib import Path
+
 import pytest
 from streamlit.testing.v1 import AppTest
+
 
 @pytest.fixture
 def mock_load_mode(monkeypatch):
     monkeypatch.setenv("LOAD_MODE", "local")
 
 
+@pytest.fixture
+def mock_config_path(monkeypatch, tmpdir):
+    mock_config = tmpdir.join("config.yaml")
+    mock_config.write("""
+cookie:
+  expiry_days: 30
+  key: dummy_signature_key
+  name: test_gas_app_login
+credentials:
+  usernames:
+    test_user:
+      email: test_user@example.com
+      failed_login_attempts: 0
+      logged_in: false
+      name: Test User
+      password: $2b$12$dummyhashedpassword
+preauthorized:
+  emails:
+  - test_user@example.com
+    """)
+    monkeypatch.setattr("utils.CONFIG_PATH", Path(mock_config))
+
+
 def test_dummy():
     assert True
 
 
-def test_home_page(mock_load_mode):
+def test_home_page(mock_load_mode, mock_config_path):
     """
     Test the home page of the application.
 
